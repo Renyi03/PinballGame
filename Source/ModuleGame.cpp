84bc75a3eea6a22ce01cd3f17f0f6055b562448b
Renyi03 <yinyeji@gmail.com>
2025-10-25 17:58:46 +0200

    fps fixed!!!!!!

--- Source/ModuleGame.cpp
+++ Source/ModuleGame.cpp
@@ -525,6 +525,33 @@ private:
 
 };
 
+class Spring : public PhysicEntity
+{
+public:
+	Spring(ModulePhysics* physics, int _x, int _y, Module* _listener, Texture2D _texture)
+		: PhysicEntity(physics->CreateCircle(_x, _y, 10), _listener)
+		, texture(_texture)
+	{
+
+	}
+
+	void Update() override
+	{
+		int x, y;
+		body->GetPhysicPosition(x, y);
+		Vector2 position{ (float)x, (float)y };
+		float scale = 1.0f;
+		Rectangle source = { 0.0f, 0.0f, (float)texture.width, (float)texture.height };
+		Rectangle dest = { position.x, position.y, (float)texture.width * scale, (float)texture.height * scale };
+		Vector2 origin = { (float)texture.width / 2.0f, (float)texture.height / 2.0f };
+		float rotation = body->GetRotation() * RAD2DEG;
+		DrawTexturePro(texture, source, dest, origin, rotation, WHITE);
+	}
+private:
+	Texture2D texture;
+
+};
+
 ModuleGame::ModuleGame(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
 	
